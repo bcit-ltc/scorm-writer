e69d92a41d5770be8ae8a8833d558be8011f02f0
Viresh Soedhwa <vsoedhwa@bcit.ca>
2025-11-07 08:59:22 -0800

    chore: remove unused pandoc conversion module and filters

--- pandoc/pandoc-filters/tables.lua
+++
@@ -1,9 +0,0 @@
-function Table(elem)
-
-	local lists = {}
-    table.insert(lists, pandoc.Plain{pandoc.RawInline('html', "<!-- START OF TABLE -->")})
-	table.insert(lists, elem)
-	table.insert(lists, pandoc.Plain{pandoc.RawInline('html', "<!-- END OF TABLE -->")})
-	return lists
-
-end
